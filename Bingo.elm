8b54697425ebfaa549fe60ef5eae78f2a1e6ad66
rolandnsharp <rolandnsharp@gmail.com>
2017-10-11 14:33:00 +1000

    piping tests

--- Bingo.elm
+++ Bingo.elm
@@ -3,5 +3,13 @@ module Bingo exposing (..)
 import Html
 
 
+-- main =
+-- Html.text (String.repeat 3 (String.toUpper "rolands game #3"))
+
+
 main =
-    Html.text "Hello world"
+    "rolands game #3 "
+        |> String.toUpper
+        |> String.repeat 3
+        |> String.pad 100 '*'
+        |> Html.text
